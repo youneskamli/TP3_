16efb707d6838de217e53680fbff8814bfffd411
youneskamli <65299386+youneskamli@users.noreply.github.com>
2022-01-19 14:30:17 +0100

    Update Magasin.h

--- Magasin.h
+++ Magasin.h
@@ -11,16 +11,9 @@ public:
 	void addProduit(Produit produit);
 	void afficheProduits();
 	void ChercheProduit(std::string produit_cherche);
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-	void updateQuantity(std::string nom ,int quantite);
-=======
 	void updateQuant(std::string nom ,int quantite);
 	void AddClient(Client client);
->>>>>>> Stashed changes
-=======
-	void updateQuant(std::string nom ,int quantite);
->>>>>>> test
+
 
 private:
 	std::vector<Produit> _produits;
